d3bbbb6e2424921485148c731a629fce323634aa
jaipranav89-debug <jaipranv89@gmail.com>
2026-03-11 11:11:06 +0530

    displaying the default name

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,16 +1,12 @@
-
 public class HelloApp {
     public static void main(String[] args) {
         
-       
         if (args.length > 0) {
-           
             String name = args[0];
             
-            
             System.out.println("Hello, " + name + "!");
-        } else {
-          
+        }
+         else {
             System.out.println("Hello, World!");
         }
     }
