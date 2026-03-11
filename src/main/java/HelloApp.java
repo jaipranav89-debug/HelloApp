b819b071cf8e3dac0eb2e27637cd32bbeb23d6d8
jaipranav89-debug <jaipranv89@gmail.com>
2026-03-11 11:16:56 +0530

    displaying multiple name

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,27 @@
 public class HelloApp {
     public static void main(String[] args) {
         
+        String finalName;
+
         if (args.length > 0) {
-            String name = args[0];
             
-            System.out.println("Hello, " + name + "!");
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            finalName = nameBuilder.toString();
+            
+        } else {
+
+            finalName = "World"; 
         }
-         else {
-            System.out.println("Hello, World!");
-        }
+
+        System.out.println("Hello, " + finalName + "!");
     }
 }
