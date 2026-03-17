521efa5d4e0ddadba2948a90e5a9c1176610923d
jaipranav89-debug <jaipranv89@gmail.com>
2026-03-17 08:54:32 +0530

    Display Hellonwith multiple args using enchanced for loop or default

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,27 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
-        
-        String finalName;
-
-        if (args.length > 0) {
-            
+ 
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
             StringBuilder nameBuilder = new StringBuilder();
-            
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-                
-                if (i < args.length - 1) {
+            boolean first = true;
+            for (String name : args) {
+                if (!first) {
                     nameBuilder.append(", ");
                 }
+                
+                nameBuilder.append(name);
+                first = false; 
             }
-
-            finalName = nameBuilder.toString();
-            
-        } else {
-
-            finalName = "World"; 
+   
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
         }
-
-        System.out.println("Hello, " + finalName + "!");
     }
 }
